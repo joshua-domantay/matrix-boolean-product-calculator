ce61be68ebe57e30a2f7adc3caee450845130356
Joshua Domantay <dom.josh2701@gmail.com>
2021-02-27 19:57:08 -0800

    Get input. Check for errors

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Joshua Anthony Domantay
 # Matrix Boolean Product Calculator
 # 27 February 2021
+
+rowsA = input("Matrix A, number of rows: ")
+colsA = input("Matrix A, number of columns: ")
+rowsB = input("Matrix B, number of rows: ")
+colsB = input("Matrix B, number of columns: ")
+
+print()
+if(not rowsA.isdigit() or not colsA.isdigit() or not rowsB.isdigit() or not colsB.isdigit()):
+    print("ERROR: At least one input is not a digit.")
+elif(colsA != rowsB):
+    print("ERROR: The number of rows from Matrix A is not equal to the number of columns in Matrix B.")
+else:
+    pass
